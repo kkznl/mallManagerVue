7a73393ebc720dea41fb0ddf11863e9be9a61b50
slk <2267076449@qq.com>
2020-12-15 15:08:43 +0800

    第一次添加的login组件

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,16 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Login from '@/components/login/login.vue'
+//@  这个标记能自动找到src文件夹
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path:'/'
+      path:'/login',
+      name:'login',
+      component:Login
     }
   ]
 })
