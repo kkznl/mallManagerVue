68f16185ecede31a192bf2b0668997f573519bc0
slk <2267076449@qq.com>
2020-12-18 16:42:13 +0800

    登陆的具体实现，await同步化异步代码

--- src/components/login/login.vue
+++ src/components/login/login.vue
@@ -14,19 +14,54 @@
   </div>
 </template>
 
+
+
 <script>
   export default {
     data() {
       return {
         username: '',
-        password: ''
+        password: '',
       }
     },
     methods:{
-      handleLogin:function(){
-        this.$http.get("login").then(res=>{
-           console.log("数据：" + res.status , res.status)
-        })
+        async handleLogin(){
+        let params = new URLSearchParams()
+        params.append("mgName",this.username)
+        params.append("mgPwd",this.password)
+        const _this = this
+
+        //async + await 将异步代码实现为同步代码
+        const res = await this.$http.post("login",params)    //async放在方法前面  await放在异步操作的前面
+
+        console.log("数据：" + res.data)
+        let msg = res.data
+        if (msg.status === 200){
+            _this.$message.success("登陆成功！")
+            //获取传过来的token,并存储在本地
+            const jwtToken = msg.token
+            localStorage.setItem("token",jwtToken)
+            //跳转主页
+            _this.$router.push({name:'home'})
+        }else{
+            _this.password = ''
+            _this.$message.error('用户名或密码错误！登陆失败！');
+        }
+
+
+        //异步代码
+        /* this.$http.post("login",params).then(res=>{
+           console.log("数据：" + res , res.status)
+           let msg = res.data
+           if (msg.status === 200){
+               _this.$message.success("登陆成功！")
+               //跳转主页
+               _this.$router.push({name:'home'})
+           }else{
+               _this.password = ''
+               _this.$message.error('用户名或密码错误！登陆失败！');
+           }
+        }) */
       }
     }
   }
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Login from '@/components/login/login.vue'
+import Home from '@/components/home/home.vue'
 //@  这个标记能自动找到src文件夹
 
 Vue.use(Router)
@@ -11,6 +12,11 @@ export default new Router({
       path:'/login',
       name:'login',
       component:Login
+    },
+    {
+      path:'/',   //根目录
+      name:'home',
+      component:Home
     }
   ]
 })
